99a4c115b68fa1ed95e938d6eec9bcacb1528976
mndowne <mndowne@gmail.com>
2016-11-17 07:30:49 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var app = express();
+
+app.set('port',(process.env.PORT || 3000));
+
+
+app.listen(app.get('port'),function(){
+  console.log('server is up on process.env.PORT');
+});
